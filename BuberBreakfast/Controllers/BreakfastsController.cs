c535f8d97193ef0840272e7288242de719b9ba6f
jameshay <james.ed.hay@gmail.com>
2023-10-12 21:44:32 +0100

    updated get request in controller to include model set up

--- BuberBreakfast/Controllers/BreakfastsController.cs
+++ BuberBreakfast/Controllers/BreakfastsController.cs
@@ -1,4 +1,5 @@
 using BuberBreakfast.Contracts.Breakfast;
+using BuberBreakfast.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BuberBreakfast.Controllers;
@@ -7,11 +8,37 @@ namespace BuberBreakfast.Controllers;
 [Route("[controller]")]
 public class BreakfastController : ControllerBase
 {
-    [HttpPost()]
+    [HttpPost]
 
     public IActionResult CreateBreakfast(CreateBreakfastRequest request)
     {
-        return Ok(request);
+        var breakfast = new Breakfast(
+            Guid.NewGuid(), 
+            request.Name,
+            request.Description,
+            request.StartDateTime,
+            request.EndDateTime,
+            DateTime.UtcNow,
+            request.Savory,
+            request.Sweet)
+
+            //TODO: save to database 
+
+            var response = new BreakfastResponse(
+                breakfast.Id,
+                breakfast.Name,
+                breakfast.Description,
+                breakfast.StartDateTime,
+                breakfast.EndDateTime,
+                breakfast.LastModifiedDateTime,
+                breakfast.Savory,
+                breakfast.Sweet
+            )
+
+        return CreatedAtAction(
+            actionName: nameof(GetBreakfast),
+            routeValues: new { id = breakfast.Id},
+            value: response);
     }
 
     [HttpGet("{id:guid}")]
