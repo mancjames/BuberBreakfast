1961d0f15e86311554ef486ace2fbd762d28ee04
jameshay <james.ed.hay@gmail.com>
2023-10-15 16:57:27 +0100

    added error handling to provide 500 error instead of exception

--- BuberBreakfast/Program.cs
+++ BuberBreakfast/Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddScoped<IBreakfastService, BreakfastService>();
 
 var app = builder.Build();
 
+app.UseExceptionHandler("/error");
 app.UseHttpsRedirection();
 app.MapControllers();
 app.Run();
